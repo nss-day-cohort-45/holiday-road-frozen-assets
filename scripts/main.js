47880854a6d59a1719f05cc7476477d6a8ba29b5
TKomrij <tatiane.b.komrij@gmail.com>
2020-12-14 11:01:42 -0600

    add attractionSelect dropdown

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 import { getParks } from "./parks/ParkProvider.js";
 import {getAttractions} from './attractions/AttractionProvider.js'
+import {attractionSelect} from './attractions/AttractionSelect.js'
 
 getParks();
 getAttractions()
+attractionSelect()
 
 
