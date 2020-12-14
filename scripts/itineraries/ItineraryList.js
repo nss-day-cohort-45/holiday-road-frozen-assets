466fa41931f4f7415737d80595177a17cf93c344
christinaashworth <christina.ashworth@ymail.com>
2020-12-14 11:04:45 -0600

    finished itinerary list

--- scripts/itineraries/ItineraryList.js
+++ scripts/itineraries/ItineraryList.js
@@ -1,9 +1,7 @@
 import { getItineraries, useItineraries } from "./ItineraryProvider.js";
 import { itineraryHTMLConverter } from "./Itinerary.js";
 
-// Query the DOM for the element that your notes will be added to 
 const contentTarget = document.querySelector(".itineraryList")
-// Define ye olde Evente Hubbe
 const eventHub = document.querySelector(".container")
 
 eventHub.addEventListener("itineraryStateChanged", () => {
@@ -12,20 +10,18 @@ eventHub.addEventListener("itineraryStateChanged", () => {
 
 const render = (itineraryArray) => {
     const itineraryStrings = itineraryArray.map(
-        // convert the notes objects to HTML with NoteHTMLConverter
         (itinerary) => {
-          return itineraryHTMLConverter(note)
+          return itineraryHTMLConverter(itinerary)
         }
     ).join("")
 
     contentTarget.innerHTML = itineraryStrings
 }
 
-// Standard list function you're used to writing by now. BUT, don't call this in main.js! Why not?
-export const NoteList = () => {
-    getNotes()
+export const itineraryList = () => {
+    getItineraries()
         .then(() => {
-            const allNotes = useNotes()
-            render(allNotes)
+            const allItineraries = useItineraries()
+            render(allItineraries)
         })
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 import { getParks } from "./parks/ParkProvider.js";
 import {getAttractions} from './attractions/AttractionProvider.js'
+import "./itineraries/ItineraryList.js"
 
 getParks();
 getAttractions()
