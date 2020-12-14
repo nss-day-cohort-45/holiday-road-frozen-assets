bd5c83b370bc90ec46d6297cf24265a449ed469e
christinaashworth <christina.ashworth@ymail.com>
2020-12-14 13:02:40 -0600

    updating main

--- scripts/itineraries/ItineraryList.js
+++ scripts/itineraries/ItineraryList.js
@@ -1,7 +1,7 @@
-import { getItineraries, useItineraries } from "./ItineraryProvider.js";
+import { getItineraries, useItineraries, saveItineraries } from "./ItineraryProvider.js";
 import { itineraryHTMLConverter } from "./Itinerary.js";
 
-const contentTarget = document.querySelector(".itineraryList")
+const contentTarget = document.querySelector(".content__saved-itineraries")
 const eventHub = document.querySelector(".container")
 
 eventHub.addEventListener("itineraryStateChanged", () => {
@@ -25,3 +25,41 @@ export const itineraryList = () => {
             render(allItineraries)
         })
 }
+let park = ``
+let attraction = ``
+let eatery = ``
+
+eventHub.addEventListener('parkChosen', event => {
+  if (event.detail.parkThatWasChosen!=="0"){
+    console.log(event.detail.parkThatWasChosen)
+    park = event.detail.parkThatWasChosen.value
+    return park
+  }
+})
+
+eventHub.addEventListener('attractionChosen', event => {
+  if (event.detail.eateryThatWasChosen!=="0"){
+    attraction = event.detail.attractionThatWasChosen.value
+    return attraction
+  }
+})
+
+eventHub.addEventListener('eateryChosen', event => {
+  if (event.detail.eateryThatWasChosen!=="0"){
+    eatery = event.detail.eateryThatWasChosen.value
+    return eatery
+  }
+})
+
+
+eventHub.addEventListener("click", clickEvent => {
+  if (clickEvent.target.id === "saveItinerary") {
+      const newItinerary = {
+          park: park,
+          attraction: attraction,
+          eatery: eatery,
+      }
+
+      saveItineraries(newItinerary)
+  }
+})
