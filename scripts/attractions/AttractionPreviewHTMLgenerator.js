6ff5c52fd14d47b0be9f4241800e0ac448225b76
TKomrij <tatiane.b.komrij@gmail.com>
2020-12-14 15:47:08 -0600

    add id to detail button

--- scripts/attractions/AttractionPreviewHTMLgenerator.js
+++ scripts/attractions/AttractionPreviewHTMLgenerator.js
@@ -5,7 +5,7 @@ export const attractionHTMLConverter = (attractionObject) => {
           <div class="attractionName">Name: ${ attractionObject.name }</div>
           <div class="attractionLocation">Location: ${attractionObject.city}, ${attractionObject.state}</div>
       </section>
-      <div class="detailButton">
+      <div class="detailButton" id="attractionDetailButton--id">
         <button class="attractionDetails">Details</button>
       </div>
   </div>
