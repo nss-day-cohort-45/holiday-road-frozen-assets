e436a41da46333edfc58a4be3b49289963e2572b
Aaron Resch <aaronmresch@gmail.com>
2020-12-14 10:24:19 -0600

    committing to save

--- scripts/parks/ParkSelect.js
+++ scripts/parks/ParkSelect.js
@@ -1,21 +1,24 @@
-import { useParks } from "./ParkProvider.js";
+import { getParks, useParks } from './ParkProvider.js';
 
 const eventHub = document.querySelector('.container');
 const parkSelectTarget = document.querySelector('.dropdowns__parks');
 
 export const parkSelect = () => {
-  let parks = useParks();
-  render(parks);
-}
+  getParks().then(() => {
+    let parks = useParks();
+    render(parks);
+  });
+};
 
 const render = (parkArray) => {
   parkSelectTarget.innerHTML = `
   <select class="dropdown" id="parkDropdown">
     <option value="0">Select a National Park...</option>
-    ${parkArray.map((park)=>`<option value="${park.id}"></option>`)
-
-
-    }
+    ${parkArray
+      .map((park) => `<option value="${park.id}">${park.fullName}</option>`)
+      .join('')}
   </select>
-  `
-}
+  `;
+};
+
+//TODO: write change event handler
