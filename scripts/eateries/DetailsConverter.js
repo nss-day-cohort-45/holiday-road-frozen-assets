4aee0a79747f2691770ddd5d845cc042dc24078f
Highwind245 <joshuahughes1426@gmail.com>
2020-12-16 10:17:45 -0600

    fixed dialog and styled for continuity.

--- scripts/eateries/DetailsConverter.js
+++ scripts/eateries/DetailsConverter.js
@@ -1,19 +1,15 @@
 export const EateryDetailsConverter = (eatery) => {
     return `
-    <dialog open class="eateryDialog">
-    ${eatery.businessName}<br>
-    Located in ${eatery.city}, ${eatery.state}.<br>
-    ${eatery.description}<br>
-    Amenities:
-    <ul>
-    <ul>Wheelchair Accesible? ${eatery.ameneties.wheelchairAccessible}</ul>
-    <ul>Pet Friendly? ${eatery.ameneties.petFriendly}</ul>
-    <ul>Wifi? ${eatery.ameneties.wifi}</ul>
-    <ul>Diaper Facility? ${eatery.ameneties.diaperFacility}</ul>
-    <ul>Playground? ${eatery.ameneties.playground}</ul>
-    <ul>Restrooms? ${eatery.ameneties.restrooms}</ul>
-    <ul>
+    <h2 class="eateryName">${eatery.businessName}</h2>
+    <p class="eateryLocation>Located in ${eatery.city}, ${eatery.state}.</p>
+    <p class="eateryDescription">${eatery.description}</p>
+    <h4 class="eateryAmenities">Amenities:</h4>
+    <p class="eatery__Wheelchair">Wheelchair Accesible? ${eatery.ameneties.wheelchairAccessible}</p>
+    <p class="eatery__pets">Pet Friendly? ${eatery.ameneties.petFriendly}<p>
+    <p class="eatery__wifi">Wifi? ${eatery.ameneties.wifi}</p>
+    <p class="eatery__changingRoom">Diaper Facility? ${eatery.ameneties.diaperFacility}</p>
+    <p class="eatery__playground">Playground? ${eatery.ameneties.playground}</p>
+    <p class="eatery__restrooms">Restrooms? ${eatery.ameneties.restrooms}</p>
     <button class="close" id="closeModal">Close</button>
-    </dialog>
     `
   }
