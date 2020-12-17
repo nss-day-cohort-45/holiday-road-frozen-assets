d06a17bac20b5d4fd451f47e136e01e0e78b8f0d
Highwind245 <joshuahughes1426@gmail.com>
2020-12-17 10:04:33 -0600

    Moved button again and changed true/false into yes/no

--- scripts/eateries/DetailsConverter.js
+++ scripts/eateries/DetailsConverter.js
@@ -1,26 +1,24 @@
-const yesOrNo = (eatery) => {
-  for (const amenity in eatery.ameneties) {
-    if (amenity === true) {
-      eatery.ameneties.amenity = "Yes"
-    }
-    else {
-      eatery.ameneties.amenety = "No"
-    }
+const yesOrNo = (amenity) => {
+  if (amenity === true) {
+    return "Yes"
+  }
+  else {
+    return "No"
   }
 }
 
 export const EateryDetailsConverter = (eatery) => {
-    return `
+  return `
+    <div class="close"><button class="close" id="closeModal">Close</button></div>
     <h2 class="eateryName">${eatery.businessName}</h2>
     <p class="eateryLocation>Located in ${eatery.city}, ${eatery.state}.</p>
     <p class="eateryDescription">${eatery.description}</p>
     <h4 class="eateryAmenities">Amenities:</h4>
-    <p class="eatery__Wheelchair">Wheelchair Accesible? ${yesOrNo(eatery.ameneties.wheelchairAccessible)}</p>
-    <p class="eatery__pets">Pet Friendly? ${yesOrNo(eatery.ameneties.petFriendly)}<p>
-    <p class="eatery__wifi">Wifi? ${yesOrNo(eatery.ameneties.wifi)}</p>
-    <p class="eatery__changingRoom">Diaper Facility? ${yesOrNo(eatery.ameneties.diaperFacility)}</p>
-    <p class="eatery__playground">Playground? ${yesOrNo(eatery.ameneties.playground)}</p>
-    <p class="eatery__restrooms">Restrooms? ${yesOrNo(eatery.ameneties.restrooms)}</p>
-    <div class="close"><button class="close" id="closeModal">Close</button></div>
+    <p class="eatery__Wheelchair">Wheelchair Accesible: ${yesOrNo(eatery.ameneties.wheelchairAccessible)}</p>
+    <p class="eatery__pets">Pet Friendly: ${yesOrNo(eatery.ameneties.petFriendly)}<p>
+    <p class="eatery__wifi">Wifi: ${yesOrNo(eatery.ameneties.wifi)}</p>
+    <p class="eatery__changingRoom">Diaper Facility: ${yesOrNo(eatery.ameneties.diaperFacility)}</p>
+    <p class="eatery__playground">Playground: ${yesOrNo(eatery.ameneties.playground)}</p>
+    <p class="eatery__restrooms">Restrooms: ${yesOrNo(eatery.ameneties.restrooms)}</p>
     `
-  }
+}
