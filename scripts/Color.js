8c7b806425494a1d15fcedadd7fd1988914a2feb
Aaron Resch <aaronmresch@gmail.com>
2020-12-18 13:27:58 -0600

    changed color picker event to 'input', now updates live

--- scripts/Color.js
+++ scripts/Color.js
@@ -1,6 +1,6 @@
 const colorPicker = document.querySelector('#changeColor');
 let root = document.documentElement
 
-colorPicker.addEventListener('change', ()=> {
+colorPicker.addEventListener('input', ()=> {
   root.style.setProperty('--secondary-bg-color', colorPicker.value);
 })
